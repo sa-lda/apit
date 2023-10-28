eb440bef65fab81ee6e78ed571a1fe8415a7c556
sa-lda <85459043+sa-lda@users.noreply.github.com>
2023-10-28 22:27:22 +0100

    Update demo.route.js

--- routes/demo.route.js
+++ routes/demo.route.js
@@ -3,6 +3,6 @@ const { SuccessResponseObject } = require('../common/http');
 
 const r = Router();
 
-r.get('/', (req, res) => res.json(new SuccessResponseObject('demo path live 🚀')));
+r.get('/', (req, res) => res.json({sa: 92});
 
 module.exports = r;
